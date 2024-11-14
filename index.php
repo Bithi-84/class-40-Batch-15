bd3e542a4fdda7c2ef43389405623c88e4bb77a7
Bithi-84 <tehzeebbithi.2016@gmail.com>
2024-11-14 15:04:10 +0600

    switch-case & PHP function

---
+++ index.php
@@ -0,0 +1,93 @@
+
+
+<?php
+
+// $marks = 80;
+
+// if($marks>=80){
+//     echo"The grade is A+";
+// }
+
+// elseif($marks>=70){
+//     echo"The grade is A";
+// }
+
+//  elseif($marks>=60){
+//     echo"The grade is A-";
+// }
+// elseif($marks>=50){
+//     echo"The grade is B";
+// }
+
+// elseif($marks>=40){
+//     echo"The grade is C";
+// }
+
+// else{
+//     echo"The grade is F";
+// }
+
+
+// $marks = 70;
+
+// switch($marks){
+//     case 80;
+//     echo "The grade is A+";
+//     break;
+
+//     case 70;
+//     echo "The grade is A"."<br>";
+//     break;
+
+    
+//     case 60;
+//     Echo "The grade is A-";
+//     break;
+    
+//     default:
+//     echo "The grade is F";
+// }
+
+
+
+// $rahim = "car";
+
+//    switch($rahim)
+//    {case "car";
+//     echo "Long tour";
+//     break;
+
+//     case "bike";
+//     echo "Hang out";
+//     break;
+
+//     case "cycle";
+//     echo "field";
+//     break;
+
+//     default:
+//     echo "Stay home";
+//     break;
+// }
+    
+function sum ($number1, $number2){
+
+    $x = $number1;
+    $y = $number2;
+    
+    $result = $x  + $y;
+    // echo $result;
+    return $result;
+}
+
+$sumvalue = sum (56, 60);
+
+$number3 = 200;
+
+$secondsumvalue = $sumvalue + $number3;
+
+echo $secondsumvalue;
+
+
+
+?>
